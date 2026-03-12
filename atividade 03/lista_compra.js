67b93031134c1693e729fd29878c35d098c04895
Clara Gomes <batistaclara923@gmail.com>
2026-03-12 12:04:35 -0300

    Create lista_compra.js

---
+++ atividade 03/lista_compra.js
@@ -0,0 +1,14 @@
+let lista_compras = ["Arroz", "Feijão", "Açúcar", "Salmão"];
+lista_compras.push("Refrigerante");
+lista_compras.push("Macarrão");
+lista_compras.push("Chocolate");
+lista_compras.forEach(item => {
+    console.log(item);
+});
+
+let cesta_frutas = ["Maçã", "Uva", "Banana", "Manga"];
+cesta_frutas.push("Maracujá");
+cesta_frutas.push("Limão");
+cesta_frutas.forEach(fruta =>{
+    console.log(fruta);
+});
